e7d91ac60a32a412a42411eaf62a64504b489f17
Ravi Nagabhyru <ravi.nagabhyru@gmail.com>
2026-03-19 13:54:53 -0400

    chore: update README admin docs, Android plugin version, and migration RLS restrictions
    - README: add administration section with SQL commands for approving users
    - sdk-android: bump Android Gradle plugin from 8.2.0 to 8.13.2
    - migration: revoke pending_approvals and approve_user access from anon/authenticated roles

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library") version "8.2.0"
+    id("com.android.library") version "8.13.2"
     id("org.jetbrains.kotlin.android") version "1.9.21"
     id("maven-publish")
 }
